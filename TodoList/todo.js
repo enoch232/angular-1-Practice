faae6351b8b6791f2db14e6fe8b5ad5e40f039a3
enoch232 <enoch232@gmail.com>
2016-05-31 23:56:05 +0900

    make finish todo method

--- TodoList/todo.js
+++ TodoList/todo.js
@@ -5,16 +5,26 @@ todoList.controller('todoController',function($scope,$http){
 		$http.get("http://localhost:3000/api/todos").success(function(response){
 			$scope.todos = response;
 		});
-	}
-
+	};
 	$scope.addTodo = function(){
 		$scope.todos.push({title: $scope.todoInput});
 		$.ajax({
 			type: "POST",
 			url: "http://localhost:3000/api/todos",
-			data: {title: $scope.todoInput},
+			data: {title: $scope.todoInput}
 		});
-	}
+		$scope.todoInput = "";
+		$("#title").focus();
+		$scope.getTodos();
+	};
+	$scope.finishTodo = function(id, title){
+		$.ajax({
+			type: "PUT",
+			url: "http://localhost:3000/api/todos/"+id,
+			data: {title: title, finished: true}
+		});
+		$scope.getTodos();
+	};
 	var init = function(){
 		$scope.getTodos();
 	};
