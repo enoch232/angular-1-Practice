d692dcc54f6be4f0f3a1f331125628de4c428455
enoch232 <enoch232@gmail.com>
2016-05-31 12:59:33 +0900

    add getTodos, and post methods

--- TodoList/todo.js
+++ TodoList/todo.js
@@ -1,4 +1,23 @@
 var todoList = angular.module('todoList',[]);
-todoList.controller('todoController',function($scope){
-	$scope.todos = ["Nothing","much","todo"];
+todoList.controller('todoController',function($scope,$http){
+	$scope.todos;
+	$scope.getTodos = function(){
+		$http.get("http://localhost:3000/api/todos").success(function(response){
+			$scope.todos = response;
+		});
+	}
+
+	$scope.addTodo = function(){
+		$scope.todos.push({title: $scope.todoInput});
+		$.ajax({
+			type: "POST",
+			url: "http://localhost:3000/api/todos",
+			data: {title: $scope.todoInput},
+		});
+	}
+	var init = function(){
+		$scope.getTodos();
+	};
+	init();
+
 });
